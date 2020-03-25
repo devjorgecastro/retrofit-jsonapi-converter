dd5eb52f9cc2a2208d2fe694b1d6e3f3c90b8a0a
Jorge Castro <dev.jorgecastro@gmail.com>
2020-03-24 20:53:58 -0500

    Add getDataObservableList and getDataSingleList methods in TestApi.kt file

--- app/src/main/java/tech/jorgecastro/jsonapi/example/TestApi.kt
+++ app/src/main/java/tech/jorgecastro/jsonapi/example/TestApi.kt
@@ -1,7 +1,10 @@
 package tech.jorgecastro.jsonapi.example
 
+import io.reactivex.Observable
+import io.reactivex.Single
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
+import tech.jorgecastro.jsonapi.JsonApiMethod
 import tech.jorgecastro.jsonapi.dto.ZoneCoverage
 
 interface TestApi {
@@ -10,6 +13,15 @@ interface TestApi {
 
     @GET("v2/5e538c4b2e000058002dac0d")
     suspend fun getData2(): Flow<List<ZoneCoverage>>
+
+
+    @JsonApiMethod
+    @GET("v2/5e538c4b2e000058002dac0d")
+    fun getDataObservableList(): Observable<List<ZoneCoverage>>
+
+    @JsonApiMethod
+    @GET("v2/5e538c4b2e000058002dac0d")
+    fun getDataSingleList(): Single<List<ZoneCoverage>>
 
     @GET("v2/5e797e302d0000ab7b18bd39")
     suspend fun getDataWithError1(): List<ZoneCoverage>
