63370f6d6c834439e222c0eb3bb78faf284ddf35
Jorge Castro <dev.jorgecastro@gmail.com>
2020-03-31 00:42:05 -0500

    Class JsonApiRetrofitCall is separated into a separate file.

--- retrofit-jsonapi-converter/src/main/java/tech/jorgecastro/jsonapi/adapter/JsonApiCallAdapter.kt
+++ retrofit-jsonapi-converter/src/main/java/tech/jorgecastro/jsonapi/adapter/JsonApiCallAdapter.kt
@@ -1,76 +1,15 @@
 package tech.jorgecastro.jsonapi.adapter
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.Request
-import okio.Timeout
-import org.json.JSONObject
-import retrofit2.*
-import tech.jorgecastro.jsonapi.JsonApiError
-import tech.jorgecastro.jsonapi.exception.JsonApiResponseException
+import retrofit2.Call
+import retrofit2.CallAdapter
 import java.lang.reflect.Type
-
-class JsonApiRetrofiCall<T>(val call: Call<T>): Call<T> {
-    @Throws(Exception::class)
-    override fun enqueue(callback: Callback<T>) {
-        call.enqueue(object: Callback<T> {
-            override fun onFailure(call: Call<T>, t: Throwable) {
-                callback.onFailure(call, t)
-            }
-
-            @Throws(Exception::class)
-            override fun onResponse(call: Call<T>, response: Response<T>) {
-                val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-                val httpCode = response.code()
-
-                if (httpCode in 400..499) {
-                    val jsonObject = JSONObject(response.errorBody()?.string())
-                    val jsonApiError = moshi.adapter(JsonApiError::class.java).fromJson(jsonObject.toString())
-                    jsonApiError?.let {
-                        callback.onFailure(call, JsonApiResponseException(message = response.message(), data = jsonApiError))
-                    }
-                }
-                else {
-                    callback.onResponse(call, response)
-                }
-            }
-
-        })
-    }
-
-    override fun isExecuted() = call.isExecuted
-
-    override fun clone(): Call<T> {
-        return call.clone()
-    }
-
-    override fun isCanceled() = call.isCanceled
-
-    override fun cancel() {
-        call.cancel()
-    }
-
-    override fun execute(): Response<T> {
-        return call.execute()
-    }
-
-    override fun request(): Request {
-        return call.request()
-    }
-
-    override fun timeout(): Timeout {
-        return call.timeout()
-    }
-
-}
 
 class JsonApiCallAdapter<T>(
     private val responseType: Type
-): CallAdapter<T, JsonApiRetrofiCall<T>> {
-    override fun adapt(call: Call<T>): JsonApiRetrofiCall<T> {
-        return JsonApiRetrofiCall(call)
+): CallAdapter<T, JsonApiRetrofitCall<T>> {
+    override fun adapt(call: Call<T>): JsonApiRetrofitCall<T> {
+        return JsonApiRetrofitCall(call)
     }
 
     override fun responseType() = responseType
-
 }
