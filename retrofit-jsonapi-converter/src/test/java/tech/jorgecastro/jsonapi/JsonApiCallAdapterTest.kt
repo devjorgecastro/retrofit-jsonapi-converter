7782a08d7d42ce4759d4fd5f1a800ed22dcec010
Jorge Castro <dev.jorgecastro@gmail.com>
2020-04-06 01:59:52 -0500

    Add Mockito 3.3.3 to project. Create JsonApiCallAdapterTest.

---
+++ retrofit-jsonapi-converter/src/test/java/tech/jorgecastro/jsonapi/JsonApiCallAdapterTest.kt
@@ -0,0 +1,42 @@
+package tech.jorgecastro.jsonapi
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Call
+import tech.jorgecastro.jsonapi.adapter.JsonApiCallAdapter
+import tech.jorgecastro.jsonapi.adapter.JsonApiRetrofitCallAdapter
+import java.lang.reflect.Type
+
+@RunWith(MockitoJUnitRunner::class)
+class JsonApiCallAdapterTest {
+
+    private val responseTypeMock = mock(Type::class.java)
+
+    private lateinit var jsonApiCallAdapter: JsonApiCallAdapter<Any>
+
+    @Mock
+    private lateinit var retrofitCallMock: Call<Any>
+
+    @Before
+    fun setupTest() {
+        jsonApiCallAdapter = JsonApiCallAdapter<Any>(responseTypeMock)
+    }
+
+    @Test
+    fun `call adapt method success`() {
+        val responseType = jsonApiCallAdapter.adapt(retrofitCallMock)
+        assert(responseType is JsonApiRetrofitCallAdapter<*>)
+    }
+
+    @Test
+    fun `call responseType method success`() {
+        val callAdapter = JsonApiCallAdapter<Any>(responseTypeMock)
+        val responseType = callAdapter.responseType()
+        assertEquals(responseType, responseTypeMock)
+    }
+}
