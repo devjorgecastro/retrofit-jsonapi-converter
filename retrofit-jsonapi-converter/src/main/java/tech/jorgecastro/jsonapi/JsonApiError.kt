c60d7ca0f972b5d0f75de35592f0fac53e3c101a
Jorge Castro <dev.jorgecastro@gmail.com>
2020-10-20 14:38:09 -0500

    Add meta and source attr in JsonApiError.kt

--- retrofit-jsonapi-converter/src/main/java/tech/jorgecastro/jsonapi/JsonApiError.kt
+++ retrofit-jsonapi-converter/src/main/java/tech/jorgecastro/jsonapi/JsonApiError.kt
@@ -31,11 +31,14 @@ class JsonApiErrorInfo {
  * from occurrence to occurrence of the problem, except for purposes of localization.
  * @property detail a human-readable explanation specific to this occurrence of the problem.
  * Like [title], this field’s value can be localized.
+ * @property meta a meta object containing non-standard meta-information about the error.
+ * @property source an object containing references to the source of the error [JsonApiSource]
  *
  *
  * At the moment links, source, meta attributes are not supported.
  *
- * @see <a href="https://jsonapi.org/format/#errors">Json Api error specification</a>
+ * @see JsonApiSource
+ * @see <a href="https://jsonapi.org/format/#errors" target="_blank">Json Api error specification</a>
  */
 @UpcomingChanges(description = "Support for links, source, meta attributes")
 class JsonApiErrorData {
@@ -44,4 +47,21 @@ class JsonApiErrorData {
     var code: String? = null
     var title: String? = null
     var detail: String? = null
+    var meta: Map<String, *>? = null
+    var source: JsonApiSource? = null
+}
+
+/**
+ * An object containing references to the source of the error,
+ * optionally including any of the following members: [pointer] and [parameter]
+ *
+ * @property pointer a JSON Pointer [RFC6901] to the associated entity in the request document
+ * [e.g. "/data" for a primary data object, or "/data/attributes/title" for a specific attribute].
+ * @property parameter a string indicating which URI query parameter caused the error.
+ *
+ * @see <a href="https://tools.ietf.org/html/rfc6901" target="_blank">RFC6901</a>
+ */
+class JsonApiSource {
+    var pointer: String? = null
+    var parameter: String? = null
 }
