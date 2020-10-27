055b82c1eea9937040e2f67e534a54de132f11e1
Jorge Castro <dev.jorgecastro@gmail.com>
2020-10-26 21:02:15 -0500

    Create TestSingleApi, TestObservableApi and TestFlowApi

---
+++ retrofit-jsonapi-converter/src/test/java/tech/jorgecastro/jsonapi/examples/api/TestApi.kt
@@ -0,0 +1,25 @@
+package tech.jorgecastro.jsonapi.examples.api
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import kotlinx.coroutines.flow.Flow
+import retrofit2.http.GET
+import tech.jorgecastro.jsonapi.JsonApiMethod
+
+interface TestSingleApi {
+    @JsonApiMethod
+    @GET("https://run.mocky.io/v3/44db10b9-9aea-4c53-aa3e-d2ef892fa859")
+    fun test(): Single<List<String>>
+}
+
+interface TestObservableApi {
+    @JsonApiMethod
+    @GET("https://run.mocky.io/v3/44db10b9-9aea-4c53-aa3e-d2ef892fa859")
+    fun test(): Observable<List<String>>
+}
+
+interface TestFlowApi {
+    @JsonApiMethod
+    @GET("https://run.mocky.io/v3/44db10b9-9aea-4c53-aa3e-d2ef892fa859")
+    fun test(): Flow<List<String>>
+}
